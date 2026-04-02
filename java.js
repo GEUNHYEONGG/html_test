e974a0d8ea3ba18e4937916dc3829b175ef7c3af
GEUNHYEONG <gim35182@g.yju.ac.kr>
2026-04-02 22:38:56 +0900

    타깃 써보기

---
+++ java.js
@@ -0,0 +1,11 @@
+let val;
+
+val = window.outerHeight;
+console.log(val);
+val = window.outerWidth;
+console.log(val);
+
+val = window.screenY;
+console.log(val);
+val = window.screenX;
+console.log(val);
